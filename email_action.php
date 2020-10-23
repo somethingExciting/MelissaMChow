98647ade35b33fd513179ba9dcdd1a2ec8767129
Melissa <m_chow5@u.pacific.edu>
2020-10-23 16:11:37 -0700

    Fixed navbar hover colors & completed thanks_sub page

--- email_action.php
+++ email_action.php
@@ -20,11 +20,11 @@
     if( empty($errors)) {
         $to = $myemail;
         $email_subject = "Contact form submission: $name";
-        $email_body = "You have received a new message.\n"."Name: $name \n"."Email: $email_address\n Message \n $message";
+        $email_body = "You have received a new message.\n"."Name: $name \n"."Email: $email_address\n Message \n $message"."#mcwebsite";
         $headers = "From: $myemail\n";
         $headers .= "Reply-To: $email_address";
         mail($to,$email_subject,$email_body,$headers);
         //redirect to the ‘thank you’ page
-        header('Location: thanks_sub.html');
+        //header('Location: thanks_sub.html');
     }
 ?>
